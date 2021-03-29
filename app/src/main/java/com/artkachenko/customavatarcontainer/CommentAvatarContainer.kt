98d187fdc0f2016dc47623b63410cae07c6757c7
ArtemiiTkachenko <artkachenko89@gmail.com>
2021-03-29 18:05:46 +0300

    Bug fixes + new functionality

--- app/src/main/java/com/artkachenko/customavatarcontainer/CommentAvatarContainer.kt
+++ app/src/main/java/com/artkachenko/customavatarcontainer/CommentAvatarContainer.kt
@@ -9,6 +9,8 @@ import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
 
 class CommentAvatarContainer @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null, deffStyle: Int = 0) : FrameLayout(context, attributeSet, deffStyle) {
 
@@ -31,7 +33,7 @@ class CommentAvatarContainer @JvmOverloads constructor(context: Context, attribu
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val desiredWidth = suggestedMinimumWidth + paddingLeft + paddingRight
+        val desiredWidth = (avatarCount  * dp(customHeight.toFloat())) + ((avatarCount - 1) * dp(overlap)) + paddingLeft + paddingRight + marginRight + marginLeft
         val desiredHeight = dp(customHeight.toFloat()) + paddingTop + paddingBottom
 
         val measuredWidth = measureDimension(desiredWidth, widthMeasureSpec)
@@ -55,7 +57,7 @@ class CommentAvatarContainer @JvmOverloads constructor(context: Context, attribu
         }
     }
 
-    fun addAvatarPlaceholders(avatarList: List<Int>?) {
+    fun addAvatarUris(avatarList: List<Int>?) {
         if (avatarList.isNullOrEmpty()) {
             visibility = View.GONE
             return
@@ -64,7 +66,20 @@ class CommentAvatarContainer @JvmOverloads constructor(context: Context, attribu
         removeAllViews()
 
         avatarList.take(avatarCount).forEachIndexed { index, i ->
-            addViewWithOverlap(i, index)
+            addViewFromUriWithOverlap(i, index)
+        }
+    }
+
+    fun addAvatarImages(views: List<ImageView>?) {
+        if (views.isNullOrEmpty()) {
+            visibility = View.GONE
+            return
+        }
+        visibility = View.VISIBLE
+        removeAllViews()
+
+        views.take(avatarCount).forEachIndexed { index, imageView ->
+            addViewWithOverlap(imageView, index)
         }
     }
 
@@ -79,17 +94,32 @@ class CommentAvatarContainer @JvmOverloads constructor(context: Context, attribu
         }
     }
 
-    private fun addViewWithOverlap(@DrawableRes drawableId: Int, index: Int) {
-        val elevation = dpF(4F - index)
+    private fun addViewFromUriWithOverlap(@DrawableRes drawableId: Int, index: Int) {
         val imageView = ImageView(context).apply {
+            setImageViewParams(this, index)
+        }
+        imageView.setImageDrawable(ContextCompat.getDrawable(context, drawableId))
+        addView(imageView)
+    }
+
+    private fun addViewWithOverlap(imageView: ImageView, index: Int) {
+        imageView.apply {
+            setImageViewParams(this, index)
+        }
+
+        addView(imageView)
+    }
+
+    private fun setImageViewParams(imageView: ImageView, index: Int) {
+        val elevation = dpF(4F - index)
+
+        imageView.apply {
             this.elevation = elevation
             val layoutParams = ViewGroup.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
             layoutParams.height = dp(customHeight.toFloat())
             layoutParams.width = dp(customHeight.toFloat())
             this.layoutParams = layoutParams
         }
-        imageView.setImageDrawable(ContextCompat.getDrawable(context, drawableId))
-        addView(imageView)
     }
 
     private fun measureDimension(desiredSize: Int, measureSpec: Int): Int {
--- app/src/main/java/com/artkachenko/customavatarcontainer/MainActivity.kt
+++ app/src/main/java/com/artkachenko/customavatarcontainer/MainActivity.kt
@@ -2,12 +2,15 @@ package com.artkachenko.customavatarcontainer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<CommentAvatarContainer>(R.id.avatar_container).addAvatarPlaceholders(generateImageList())
+        findViewById<CommentAvatarContainer>(R.id.avatar_container).addAvatarUris(generateImageList())
+        findViewById<CommentAvatarContainer>(R.id.avatar_container_from_images).addAvatarImages(generateImages())
     }
 
     private fun generateImageList(): List<Int> {
@@ -18,5 +21,16 @@ class MainActivity : AppCompatActivity() {
             R.drawable.ic_circular_avatar_blue,
             R.drawable.ic_circular_avatar,
             R.drawable.ic_circular_avatar_red)
+    }
+
+    private fun generateImages() : List<ImageView> {
+        val reversedDrawableList = generateImageList().reversed()
+        val imageList = mutableListOf<ImageView>()
+        reversedDrawableList.forEachIndexed { index, i ->
+            imageList.add(ImageView(this).apply {
+                setImageDrawable(ContextCompat.getDrawable(this@MainActivity, reversedDrawableList[index]))
+            })
+        }
+        return imageList
     }
 }
